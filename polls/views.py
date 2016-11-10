ae95010d500f17ca49aa6f56ead2efdd84116e1f
Edward Lim <sgedwardlim@gmail.com>
2016-11-09 17:23:11 -0800

    Added new Views

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Index page")
+
+
+def detail(request, question_id):
+    return HttpResponse("you are looking at the details of " + str(question_id))
+
+
+def results(request, question_id):
+    return HttpResponse("you are looking at the results of " + str(question_id))
+
+
+def vote(request, question_id):
+    return HttpResponse("you are looking at the vote of " + str(question_id))
