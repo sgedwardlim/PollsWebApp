0dc107489992becb9a6b968cf459b4d4a89fa0ab
Edward Lim <sgedwardlim@gmail.com>
2016-11-13 21:15:02 -0800

    Added generic views for polls ap

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,34 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list, }
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    # By default rails recognizes the lower-cased version of the Model as object in specified view
+    # In this case because its a list view, the default is question_list
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Returns the last 5 published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    # By default rails recognizes the lower-cased version of the Model as object in specified view
+    # To override name, use context_object_name = 'name_to_use_instead'
+    template_name = 'polls/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    # By default rails recognizes the lower-cased version of the Model as object in specified view
+    # To override name, use context_object_name = 'name_to_use_instead'
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
